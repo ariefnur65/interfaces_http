64005838b64231c3f08351438643bccbc9ea61b1
arief.prakoso <ariefnur652010@gmail.com>
2024-12-05 06:31:48 +0700

    use reader interface and store the result to byteslice

--- main.go
+++ main.go
@@ -12,5 +12,10 @@ func main() {
 		fmt.Println("Error", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	//fmt.Println(resp)
+	bs := make([]byte, 99999) //create byteslice that has 99999 empty bytes,
+	// it needs to be large because Reader interface cannot modify slices size,
+	// when it is full, it will stop reading when the slice is full instead
+	resp.Body.Read(bs)
+	fmt.Println("Hasil Read:", string(bs))
 }
